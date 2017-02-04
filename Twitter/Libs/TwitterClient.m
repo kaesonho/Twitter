b91ccc38abc68cd89b85a0b1fd7e0c3fc987529c
kaesonho <kaesonho@yahoo-inc.com>
2017-02-04 10:54:23 -0800

    remove debug msg

--- Twitter/Libs/TwitterClient.m
+++ Twitter/Libs/TwitterClient.m
@@ -102,9 +102,7 @@ NSString * const kTwitterBaseUrl = @"https://api.twitter.com";
     [self GET:@"1.1/statuses/user_timeline.json" parameters: @{@"screen_name": screenName} progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         {
             NSArray *tweets;
-            NSLog(@"????%@", responseObject);
             tweets = [Tweet tweetsWithArray:responseObject];
-            NSLog(@"ok%@", tweets);
             completion(tweets, nil);
         }
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
